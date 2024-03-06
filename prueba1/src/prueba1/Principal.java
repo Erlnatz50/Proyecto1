58fdad61af6efeb53974902bf689deec9b6c81dc
UsuErlantz <erlantz@gmail.com>
2024-03-06 10:12:27 +0100

    Segunda validacion clase Principal

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -4,8 +4,9 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
-		System.out.println("Segunda Validacion");
-		
+		System.out.println("Hola mundo java-git");
+		System.out.println("Adios");
+		System.out.println("Pruebas Git. Se añade Cambio1");
 	}
 
 }
